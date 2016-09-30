f237fe51f6c29bbfd0d61fd7859859edebaeb017
senacarr <senansenanc@gmail.com>
2016-09-30 23:23:49 +0100

    intiial commit

---
+++ src/main/java/assignment1/CT417_SenanC_Part2/App.java
@@ -0,0 +1,13 @@
+package assignment1.CT417_SenanC_Part2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
