43ef44e31ae137426f99c1d1f81c63fe69e2cbde
senacarr <senansenanc@gmail.com>
2016-09-30 23:57:50 +0100

    Assignment 1 part 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CT417-SenanC-Part2
+> jar target/CT417-SenanC-Part2-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/assignment1/CT417_SenanC_Part2/StudentRegSystemDriver.java
@@ -0,0 +1,12 @@
+package assignment1.CT417_SenanC_Part2;
+
+import assignment1.CT417_SenanC_Part1.*;
+
+public class StudentRegSystemDriver 
+{
+    public static void main( String[] args )
+    {
+    	Student s = new Student("Senan", null, 21);
+        System.out.println("name: " + s.getStudentName());
+    }
+}
