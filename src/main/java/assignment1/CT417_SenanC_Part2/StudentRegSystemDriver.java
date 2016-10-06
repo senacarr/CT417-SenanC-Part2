ffcc8d099b38c8fc37e26ef7e6eae7473e2c4f22
senacarr <senansenanc@gmail.com>
2016-10-06 19:44:40 +0100

    Update driver class to actually do something

--- src/main/java/assignment1/CT417_SenanC_Part2/StudentRegSystemDriver.java
+++ src/main/java/assignment1/CT417_SenanC_Part2/StudentRegSystemDriver.java
@@ -1,12 +1,57 @@
 package assignment1.CT417_SenanC_Part2;
 
 import assignment1.CT417_SenanC_Part1.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class StudentRegSystemDriver 
 {
     public static void main( String[] args )
+    {   	
+    	ArrayList<Student> studentList = makeStudents(10);
+    	for(Student s : studentList)
+    	{
+    		System.out.println("Name: "       + s.getStudentName()
+    		                 + "\nAge: "      + s.getStudentAge()
+    		                 + "\nDOB: "      + s.getStudentDOB()
+    		                 + "\nID : "      + s.getStudentID()
+    		                 + "\nUserName: " + s.getUsername()
+    		                 + "\n\n***************************");    		
+    	}
+    	
+    }
+    
+    private static ArrayList<Student> makeStudents(int num)
     {
-    	Student s = new Student("Senan", null, 21);
-        System.out.println("name: " + s.getStudentName());
+    	if(num <= 0)
+    		num = 100;
+    	
+    	Random r = new Random();
+
+    	ArrayList<String> names = new ArrayList<String>();
+    	while(names.size() < num)
+    	{
+    		names.add("I'm Creative" + r.nextInt(num));
+    	}
+
+    	ArrayList<Calendar> dates = new ArrayList<Calendar>();
+    	while(dates.size() < num)
+    	{
+    		dates.add(new GregorianCalendar((2000 - r.nextInt(20)), r.nextInt(12), r.nextInt(28)));
+    	}
+    	
+    	ArrayList<Student> students = new ArrayList<Student>();
+    	for(int i = 0; i < num; i++)
+    	{
+    		try {
+				students.add(new Student(names.get(i), dates.get(i)));
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+    	}
+    	
+    	return students;
     }
 }
